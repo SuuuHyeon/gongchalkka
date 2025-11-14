bb5c39138c34b69d7b0824200317ecd59235de87
suhyeon <tngutnqls6909@gmail.com>
2025-11-14 14:03:28 +0900

    Project commit

---
+++ src/main/java/com/project/gongchalkka/GongchalkkaApplication.java
@@ -0,0 +1,13 @@
+package com.project.gongchalkka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongchalkkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongchalkkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/gongchalkka/GongchalkkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gongchalkka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongchalkkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
